e6bbf1568a181604974f1a33e7042ff9d48ac4ca
Antonio Carlos Souza Lino <antoniomotoe4plus@gmail.com>
2024-12-21 07:58:29 -0300

    Add files via upload

---
+++ Curso/ksite/js/jert.js
@@ -0,0 +1,17 @@
+let num = [5,8,2,9,3]
+num[3] = 6
+num.push(7)
+num.sort()
+console.log(`squencia ${num} é ${num.length}`)
+let pos = num.indexOf(5)
+console.log(`o valor estar nessa posição ${pos}`)
+
+/*for(let pos = 0; pos < vetores.length;pos++){
+    vetores.sort()
+    console.log(`A posição ${pos} tem o valor ${vetores[pos]} `)
+}*/
+
+/*let vetores = [8, 1, 7, 4, 2, 9]
+}
+for( let pos in vetores) {
+   //console.log(`A posiçao ${pos} tem valor ${vetores[pos]}`)*/
